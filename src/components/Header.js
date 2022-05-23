953da8e9e512977074fda0893c728cde708d8798
naiiiden <na4ko359@gmail.com>
2022-05-23 19:24:11 +0300

    hamburger button now opens and closes properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Logo from "../images/Lobe_logo.png";
 import Open from "../images/Hamburger.svg";
 import Close from "../images/Close.svg";
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const ToggleNav = () => setOpenMenu(!openMenu);
+
     return (
         <header className="header">
             <img src={Logo} alt="Lobe's logo" className="header--logo"/>
-            <button aria-label="toggle navigation" className="header--menu--button">
+            <button aria-label="toggle navigation" className="header--menu--button" onClick={ToggleNav}>
                 <img src={Open} alt=""/>
             </button>
             <nav className="header--nav">
-                <ul className="header--nav--ul">
+                <ul className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
                     <li><a href="">Overview</a></li>
                     <li><a href="">Examples</a></li>
                     <li><a href="" className="header--nav--ul--tour--link">Tour</a></li>
