9fb0ce2b374b29ee4d79dc0f6aa1084701858c33
naiiiden <na4ko359@gmail.com>
2022-05-25 06:57:59 +0300

    cta ta component styled on mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import CallToAction from "./components/CallToAction";
 import "./style/general.css";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Header/>
       <Hero h1Text="Lobe" spanText="Tour" pText="Build your first machine learning model in ten minutes. No code or experience required." embedId="Mdcw3Sb98DA"/>
+      <CallToAction h2Text="Train your app with Lobe" buttonText="Download"/>
     </div>
   )
 }
