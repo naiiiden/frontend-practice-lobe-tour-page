2895f977a51008b45f680bfec5cf250781b0ccbd
naiiiden <na4ko359@gmail.com>
2022-05-26 10:34:12 +0300

    footer: fill empty href attributes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,31 +16,31 @@ const Footer = () => {
                 <div className="footer--nav--ul--subcontainer">
                     <ul className="footer--nav--ul">
                         <li className="footer--nav--ul--heading">About</li>
-                        <li><a href="">Download</a></li>
-                        <li><a href="">Overview</a></li>
-                        <li><a href="">Examples</a></li>
-                        <li><a href="">Blog</a></li>
+                        <li><a href="https://www.lobe.ai/tour#signup">Download</a></li>
+                        <li><a href="https://www.lobe.ai/">Overview</a></li>
+                        <li><a href="https://www.lobe.ai/examples">Examples</a></li>
+                        <li><a href="https://www.lobe.ai/blog">Blog</a></li>
                     </ul>
                     <ul className="footer--nav--ul">
                         <li className="footer--nav--ul--heading">General</li>
-                        <li><a href="">Notice</a></li>
-                        <li><a href="">License</a></li>
-                        <li><a href="">Press Inquiry</a></li>
-                        <li><a href="">Press Images</a></li>
+                        <li><a href="https://www.lobe.ai/NOTICE.txt">Notice</a></li>
+                        <li><a href="https://www.lobe.ai/license">License</a></li>
+                        <li><a href="https://news.microsoft.com/microsoft-public-relations-contacts/">Press Inquiry</a></li>
+                        <li><a href="https://www.lobe.ai/assets/press/lobe-press-images.zip">Press Images</a></li>
                     </ul>
                 </div>
                 <div className="footer--resources--socials--container">
                     <ul className="footer--nav--ul">
                         <li className="footer--nav--ul--heading">Resources</li>
-                        <li><a href="">Help</a></li>
-                        <li><a href="">Tour</a></li>
-                        <li><a href="">Contact</a></li>
-                        <li><a href="">Privacy</a></li>
+                        <li><a href="https://www.lobe.ai/docs/welcome/welcome">Help</a></li>
+                        <li><a href="https://www.lobe.ai/tour">Tour</a></li>
+                        <li><a href="mailto:lobeai@microsoft.com">Contact</a></li>
+                        <li><a href="https://go.microsoft.com/fwlink/?LinkId=521839">Privacy</a></li>
                     </ul>
                     <ul className="footer--nav--ul--socials">
-                        <li><a href=""><img src={Reddit} alt=""/></a></li>
-                        <li><a href=""><img src={Twitter} alt=""/></a></li>
-                        <li><a href=""><img src={Youtube} alt=""/></a></li>
+                        <li><a href="https://reddit.com/r/lobe"><img src={Reddit} alt=""/></a></li>
+                        <li><a href="https://twitter.com/lobe_ai"><img src={Twitter} alt=""/></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCYryctkBIcCszPAFg79rwAw"><img src={Youtube} alt=""/></a></li>
                     </ul>
                 </div>
             </nav>
