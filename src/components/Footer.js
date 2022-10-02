7f164306b2dabd3d37e26adbd1eb7bc4aa961ec0
naiiiden <na4ko359@gmail.com>
2022-10-02 15:53:48 +0300

    footer nav ul refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,29 +14,35 @@ const Footer = () => {
             </ul>
             <nav className="footer--nav" aria-label="Footer">
                 <div className="footer--nav--ul--subcontainer">
-                    <ul role="menubar" className="footer--nav--ul" >
-                        <li className="footer--nav--ul--heading">About</li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/tour#signup">Download</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/">Overview</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/examples">Examples</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/blog">Blog</a></li>
-                    </ul>
-                    <ul role="menubar" className="footer--nav--ul">
-                        <li className="footer--nav--ul--heading">General</li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/NOTICE.txt">Notice</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/license">License</a></li>
-                        <li role="none"><a role="menuitem" href="https://news.microsoft.com/microsoft-public-relations-contacts/">Press Inquiry</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/assets/press/lobe-press-images.zip">Press Images</a></li>
-                    </ul>
+                    <div className="footer--nav--ul">
+                        <h3 className="footer--nav--ul--heading">About</h3>
+                        <ul role="menubar" className="footer--nav--ul" >
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/tour#signup">Download</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/">Overview</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/examples">Examples</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/blog">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer--nav--ul">
+                        <h3 className="footer--nav--ul--heading">General</h3>
+                        <ul role="menubar" className="footer--nav--ul">
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/NOTICE.txt">Notice</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/license">License</a></li>
+                            <li role="none"><a role="menuitem" href="https://news.microsoft.com/microsoft-public-relations-contacts/">Press Inquiry</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/assets/press/lobe-press-images.zip">Press Images</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="footer--resources--socials--container">
-                    <ul role="menubar" className="footer--nav--ul">
-                        <li className="footer--nav--ul--heading">Resources</li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/docs/welcome/welcome">Help</a></li>
-                        <li role="none"><a role="menuitem" href="https://www.lobe.ai/tour">Tour</a></li>
-                        <li role="none"><a role="menuitem" href="mailto:lobeai@microsoft.com">Contact</a></li>
-                        <li role="none"><a role="menuitem" href="https://go.microsoft.com/fwlink/?LinkId=521839">Privacy</a></li>
-                    </ul>
+                    <div className="footer--nav--ul">
+                        <h3 className="footer--nav--ul--heading">Resources</h3>
+                        <ul role="menubar" className="footer--nav--ul">
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/docs/welcome/welcome">Help</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.lobe.ai/tour">Tour</a></li>
+                            <li role="none"><a role="menuitem" href="mailto:lobeai@microsoft.com">Contact</a></li>
+                            <li role="none"><a role="menuitem" href="https://go.microsoft.com/fwlink/?LinkId=521839">Privacy</a></li>
+                        </ul>
+                    </div>
                     <ul role="menubar" className="footer--nav--ul--socials">
                         <li role="none"><a role="menuitem" href="https://reddit.com/r/lobe"><img src={Reddit} alt="Our Reddit"/></a></li>
                         <li role="none"><a role="menuitem" href="https://twitter.com/lobe_ai"><img src={Twitter} alt="Our Twitter"/></a></li>
