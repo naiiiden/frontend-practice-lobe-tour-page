9d71a459cd6edba7a307c440ce41a813f5e42d2d
naiiiden <na4ko359@gmail.com>
2022-10-02 15:48:27 +0300

    nav > ul add aria-* attributes

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,13 +14,13 @@ const Header = () => {
                 <img src={!openMenu ? Open : Close} alt=""/>
             </button>
             <nav className="header--nav">
-                <ul className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
-                    <li className="header--nav--ul--li--link1"><a href="https://www.lobe.ai/">Overview</a></li>
-                    <li><a href="https://www.lobe.ai/examples">Examples</a></li>
-                    <li><a href="https://www.lobe.ai/tour" className="header--nav--ul--tour--link">Tour</a></li>
-                    <li><a href="https://www.lobe.ai/blog">Blog</a></li>
-                    <li><a href="https://www.lobe.ai/docs/welcome/welcome">Help</a></li>
-                    <li className="header--nav--ul--li--download"><button className="header--nav--ul--download--link">Download</button></li>
+                <ul role="menubar" className={`header--nav--ul ${!openMenu ? "" : "show"}`}>
+                    <li role="none" className="header--nav--ul--li--link1"><a href="https://www.lobe.ai/">Overview</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.lobe.ai/examples">Examples</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.lobe.ai/tour" className="header--nav--ul--tour--link">Tour</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.lobe.ai/blog">Blog</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.lobe.ai/docs/welcome/welcome">Help</a></li>
+                    <li role="none" className="header--nav--ul--li--download"><button className="header--nav--ul--download--link">Download</button></li>
                 </ul>
             </nav>
         </header>
