d98d86da00bfb7849dd3916b2d696a0bce68fce5
naiiiden <na4ko359@gmail.com>
2022-10-02 15:59:05 +0300

    add <main> tag

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ const App = () => {
   return (
     <div>
       <Header/>
-      <Hero h1Text="Lobe" spanText="Tour" pText="Build your first machine learning model in ten minutes. No code or experience required." embedId="Mdcw3Sb98DA"/>
-      <CallToAction h2Text="Train your app with Lobe" buttonText="Download"/>
+      <main>
+        <Hero h1Text="Lobe" spanText="Tour" pText="Build your first machine learning model in ten minutes. No code or experience required." embedId="Mdcw3Sb98DA"/>
+        <CallToAction h2Text="Train your app with Lobe" buttonText="Download"/>
+      </main>
       <Footer/>
     </div>
   )
